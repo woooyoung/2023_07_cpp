78146ab68eac576aa98839dacd547ed31dbe65d2
woooyoung <axdsw121@gmail.com>
2023-07-31 14:13:14 +0900

    Bitwise_Operation 02

--- Bitwise_Operation/Bitwise_Operation/소스.cpp
+++ Bitwise_Operation/Bitwise_Operation/소스.cpp
@@ -1,9 +1,28 @@
-// 비트와이즈 연산 -> 비트를 기준으로 연산
+// 비트와이즈 연산 -> 비트를 기준으로 연산 -> 기계의 근본에 해당하는 연산
 // Bitwise or -> |
 // Bitwise and -> &
-// Shift
+// Shift ->			<<, >>
+
+//연산 속도(빠른순으로) : 더하기 + 빼기 -  곱하기 *  나누기 / 
+//						: 정수연산 실수연산
 
 #include <cstdio>
+void exam1(int params) {
+	int category1 = 1; // 1
+	int category2 = 1 << 1; // 2
+	int category3 = 1 << 2; // 4
+
+	if ((category1 & params) != 0) {
+		printf("카테고리 1 에 포함\n");
+	}
+	if ((category2 & params) != 0) {
+		printf("카테고리 2 에 포함\n");
+	}
+	if ((category3 & params) != 0) {
+		printf("카테고리 3 에 포함\n");
+	}
+}
+
 
 int main() {
 
@@ -11,10 +30,19 @@ int main() {
 	int v2 = 3; // 0011
 
 	int bitwise_or = v1 | v2;
-	printf("%d\n", bitwise_or);
+	printf("v1 | v2 : %d\n", bitwise_or);
 
 	int bitwise_and = v1 & v2;
-	printf("%d\n", bitwise_and);
+	printf("v1 & v2 : %d\n", bitwise_and);
+
+	int shift_left = v2 << 1; // 0110
+	printf("v2 << 1 : %d\n", shift_left);
+
+	int shift_left2 = v2 << 2; // 1100
+	printf("v2 << 2 : %d\n", shift_left2);
+
+	int shift_right = v2 >> 1; // 0001
+	printf("v2 >> 1 : %d\n", shift_right);
 
 	/*int v1 = 1;
 	int v2 = 2; // 0010
@@ -25,6 +53,17 @@ int main() {
 	int bitwise_and = v1 & v2;
 	printf("%d\n", bitwise_and);*/
 
+	int mario_category = 8 + 4 + 2 + 1;
+
+	int enemy_category = 0 + 4 + 2 + 1;
+
+	int turtle_category = 0 + 0 + 2 + 1;
+
+	int feature_category = 0 + 4 + 0 + 0;
+
+	exam1(1 | 2 | 4);
+
+	exam1(1 | 4);
 
 	return 0;
 }
---
+++ Bitwise_Operation/Bitwise_Operation/소스1.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+// 카테고리 분류 -> 게임의 물리엔진이나 물리 분야의 시뮬레이션(가장 빠르기 때문에)
+// 충돌체크 또는 반발력, 마찰력 체크
+int main() {
+
+	int mario_category = 8 + 4 + 2 + 1;
+
+	int enemy_category = 0 + 4 + 2 + 1;
+
+	int turtle_category = 0 + 0 + 2 + 1;
+
+	int feature_category = 0 + 4 + 0 + 0;
+
+	return 0;
+}
